30f2afa1f7d4cef31ba5ea4f151ece9233200ce3
vieirapcm <vieirapcm@gmail.com>
2022-05-29 19:15:15 -0300

    Add API project

---
+++ BestCostRouteFinder.API/Controllers/V1/RouteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BestCostRouteFinder.API.Controllers.V1
+{
+    [ApiController]
+    [Route("v1/routes")]
+    public class RouteController : ControllerBase
+    {
+        [HttpGet("")]
+        public List<string> GetAvailableRoutes()
+        {
+            List<string> availableRoutes = new()
+            {
+                "Origem: GRU, Destino: BRC, Valor: 10",
+                "Origem: BRC, Destino: SCL, Valor: 5",
+                "Origem: GRU, Destino: CDG, Valor: 75",
+                "Origem: GRU, Destino: SCL, Valor: 20",
+                "Origem: GRU, Destino: ORL, Valor: 56",
+                "Origem: ORL, Destino: CDG, Valor: 5",
+                "Origem: SCL, Destino: ORL, Valor: 20"
+            };
+
+            return availableRoutes;
+        }
+    }
+}
