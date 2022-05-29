b9c71eb125c209285850fed5f53c3dd50b0bd05a
vieirapcm <vieirapcm@gmail.com>
2022-05-29 19:54:08 -0300

    Add Infrasctructure project

---
+++ BestCostRouteFinder.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BestCostRouteFinder.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(_configuration.GetConnectionString("SqlServer"));
+        }
+    }
+}
