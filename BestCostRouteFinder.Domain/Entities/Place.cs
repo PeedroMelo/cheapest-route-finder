47acb1a751093b30857a1220a923203e04ea7901
vieirapcm <vieirapcm@gmail.com>
2022-05-29 20:05:45 -0300

    Fix OutputType

---
+++ BestCostRouteFinder.Domain/Entities/Place.cs
@@ -0,0 +1,15 @@
+﻿namespace BestCostRouteFinder.Domain.Entities
+{
+    public class Place
+    {
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public Place(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ BestCostRouteFinder.Domain/Entities/Route.cs
@@ -0,0 +1,21 @@
+﻿namespace BestCostRouteFinder.Domain.Entities
+{
+    public class Route
+    {
+        public int Id { get; }
+
+        public string Origin { get; }
+
+        public string Destiny { get; }
+
+        public decimal Cost { get; }
+
+        public Route(int id, string origin, string destiny, decimal cost)
+        {
+            Id = id;
+            Origin = origin;
+            Destiny = destiny;
+            Cost = cost;
+        }
+    }
+}
