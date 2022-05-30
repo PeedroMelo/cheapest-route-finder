74cd9eae2da707e0a3e59176a8b610d4a38442dd
vieirapcm <vieirapcm@gmail.com>
2022-05-29 23:33:33 -0300

    Add dummy get controlelr

--- BestCostRouteFinder.API/Controllers/V1/RouteController.cs
+++ BestCostRouteFinder.API/Controllers/V1/RouteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BestCostRouteFinder.Domain.AggregateModels.Route;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 namespace BestCostRouteFinder.API.Controllers.V1
@@ -7,19 +8,17 @@ namespace BestCostRouteFinder.API.Controllers.V1
     [Route("v1/routes")]
     public class RouteController : ControllerBase
     {
-        [HttpGet("")]
-        public List<string> GetAvailableRoutes()
+        private readonly IRouteRepository _routeRepository;
+
+        public RouteController(IRouteRepository routeRepository)
         {
-            List<string> availableRoutes = new()
-            {
-                "Origem: GRU, Destino: BRC, Valor: 10",
-                "Origem: BRC, Destino: SCL, Valor: 5",
-                "Origem: GRU, Destino: CDG, Valor: 75",
-                "Origem: GRU, Destino: SCL, Valor: 20",
-                "Origem: GRU, Destino: ORL, Valor: 56",
-                "Origem: ORL, Destino: CDG, Valor: 5",
-                "Origem: SCL, Destino: ORL, Valor: 20"
-            };
+            _routeRepository = routeRepository;
+        }
+
+        [HttpGet("")]
+        public IEnumerable<Route> GetAvailableRoutes()
+        {
+            IEnumerable<Route> availableRoutes = _routeRepository.GetAll();
 
             return availableRoutes;
         }
