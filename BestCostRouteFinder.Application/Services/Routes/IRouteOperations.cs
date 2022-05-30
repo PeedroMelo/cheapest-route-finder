be369fd477e9d6295935e59d4838830ad3e6c782
vieirapcm <vieirapcm@gmail.com>
2022-05-29 23:39:32 -0300

    Rename application layer

---
+++ BestCostRouteFinder.Application/Services/Routes/IRouteOperations.cs
@@ -0,0 +1,27 @@
+﻿using BestCostRouteFinder.Domain.AggregateModels.Route;
+using System.Collections.Generic;
+
+namespace BestCostRouteFinder.Application.Services.Routes
+{
+    public interface IRouteOperations
+    {
+        /// <summary>
+        /// Gets the available routes
+        /// </summary>
+        /// <returns>The list of available routes</returns>
+        IEnumerable<Route> GetAvailableRoutes();
+
+        /// <summary>
+        /// Creates a new route
+        /// </summary>
+        /// <param name="route">The new route to be created</param>
+        /// <returns>The new created route</returns>
+        Route CreateRoute(Route route);
+
+        /// <summary>
+        /// Deletes a route by the provided id
+        /// </summary>
+        /// <param name="id">The route id to be deleted</param>
+        void DeleteRoute(int id);
+    }
+}
---
+++ BestCostRouteFinder.Application/UseCases/V1/RouteOperations.cs
@@ -0,0 +1,25 @@
+﻿using BestCostRouteFinder.Application.Services.Routes;
+using BestCostRouteFinder.Domain.AggregateModels.Route;
+using System;
+using System.Collections.Generic;
+
+namespace BestCostRouteFinder.Application.UseCases.V1
+{
+    public class RouteOperations : IRouteOperations
+    {
+        public Route CreateRoute(Route route)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRoute(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Route> GetAvailableRoutes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
