5ff47f78a3dc9e239dc332079542b2f5c4ed29b0
vieirapcm <vieirapcm@gmail.com>
2022-05-31 05:10:51 -0300

    Add unit test

---
+++ BestCostRouteFinder.Domain.Test/AggregateModels/RouteTest.cs
@@ -0,0 +1,22 @@
+using BestCostRouteFinder.Domain.AggregateModels.Route;
+using Xunit;
+
+namespace BestCostRouteFinder.Domain.Test.AggregateModels
+{
+    public class RouteTest
+    {
+        [Fact]
+        public void RouteEntity_WithValidConstructor_ShouldAssingValuesWithoutException()
+        {
+            Route testRoute = new(
+                origin: "ORT",
+                destiny: "DRT",
+                cost: 100
+            );
+
+            Assert.Equal("ORT", testRoute.Origin);
+            Assert.Equal("DRT", testRoute.Destiny);
+            Assert.Equal(100, testRoute.Cost);
+        }
+    }
+}
