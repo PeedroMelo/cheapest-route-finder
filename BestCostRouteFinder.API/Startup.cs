6cb36e9942d5e38119d31cb35f3f5179e8a1adcb
vieirapcm <vieirapcm@gmail.com>
2022-05-31 02:18:26 -0300

    Remove HTPPS redirection

--- BestCostRouteFinder.API/Startup.cs
+++ BestCostRouteFinder.API/Startup.cs
@@ -55,7 +55,7 @@ namespace BestCostRouteFinder.API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BestCostRouteFinder.API v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
